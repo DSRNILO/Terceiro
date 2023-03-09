c69f82935cd752a6a3cdb513af3bb0e4f2d032cf
Danilo Rufino <81395349+DSRNILO@users.noreply.github.com>
2023-03-09 08:59:54 -0300

    Add files via upload

---
+++ Terceiro/lib/aluno_detalhe.dart
@@ -0,0 +1,47 @@
+import "package:flutter/material.dart";
+import "aluno.dart";
+
+class AlunoDetalhe extends StatefulWidget {
+  final Aluno aluno;
+
+  const AlunoDetalhe({
+    Key? key,
+    required this.aluno,
+  }) : super(key: key);
+
+  @override
+  _AlunoDetalheState createState() {
+    return _AlunoDetalheState();
+  }
+}
+
+class _AlunoDetalheState extends State<AlunoDetalhe> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.aluno.nome),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+                height: 300,
+                width: double.infinity,
+                child: Image.network(widget.aluno.urlImage)),
+            Text(widget.aluno.descricao),
+            Expanded(
+                child: ListView.builder(
+              padding: const EdgeInsets.all(7.0),
+              itemCount: widget.aluno.disciplinas.length,
+              itemBuilder: (BuildContext context, int index) {
+                final disciplina = widget.aluno.disciplinas[index];
+                return Text('${disciplina.nota}');
+              },
+            )),
+          ],
+        ),
+      ),
+    );
+  }
+}
